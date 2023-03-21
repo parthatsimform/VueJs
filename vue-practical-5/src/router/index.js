379da6cb3d006cde8879b4129944d2bf9e235c93
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-03-21 10:31:01 +0530

    Start practical-5 by creating vite project and take code from practical-4

---
+++ vue-practical-5/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+
+const routes = [
+	{
+		path: "/",
+		name: "home",
+		component: Home,
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: Login,
+	},
+	{
+		path: "/register",
+		name: "register",
+		component: Register,
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+	linkExactActiveClass: "activeBtn",
+});
+
+export default router;
