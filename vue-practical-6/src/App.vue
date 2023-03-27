1f003bbab03c7e33e91fa82dc49e4b91d3111955
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-03-27 18:48:05 +0530

    Init branch from practical-5 and changes  to emit events

---
+++ vue-practical-6/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <NavBar />
+  <RouterView />
+</template>
+
+<script>
+
+export default {
+  name: "App",
+}
+</script>
---
+++ vue-practical-6/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import NavBar from "./components/NavBar.vue";
+
+const app = createApp(App);
+app.component("NavBar", NavBar);
+app.use(router);
+app.mount("#app");
