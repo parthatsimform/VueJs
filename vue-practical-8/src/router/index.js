da3e33bedc5163a363f3f1d2d3c623065403344b
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-04-11 16:34:50 +0530

    Add lazy routing, add beforeenter instead of beforeeach

--- vue-practical-8/src/router/index.js
+++ vue-practical-8/src/router/index.js
@@ -1,35 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-import CarDetail from "../views/CarDetail.vue";
-// import { useUserStore } from "../stores/user";
 
 const routes = [
 	{
 		path: "/",
 		name: "home",
 		component: Home,
-		meta: {
-			needAuthentication: true,
+		beforeEnter: (to, from, next) => {
+			if (
+				window.localStorage.getItem("isLoggedIn") &&
+				window.localStorage.getItem("token")
+			) {
+				next();
+			} else {
+				next("/login");
+			}
 		},
 	},
 	{
 		path: "/login",
 		name: "login",
-		component: Login,
+		component: () => import("../views/Login.vue"),
 	},
 	{
 		path: "/register",
 		name: "register",
-		component: Register,
+		component: () => import("../views/Register.vue"),
 	},
 	{
 		path: "/details/:id",
 		name: "details",
-		component: CarDetail,
-		meta: {
-			needAuthentication: true,
+		component: () => import("../views/CarDetail.vue"),
+		beforeEnter: (to, from, next) => {
+			if (
+				window.localStorage.getItem("isLoggedIn") &&
+				window.localStorage.getItem("token")
+			) {
+				next();
+			} else {
+				next("/login");
+			}
 		},
 	},
 ];
@@ -38,21 +48,6 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
 	linkExactActiveClass: "activeBtn",
-});
-
-router.beforeEach((to, from, next) => {
-	if (!to.meta.needAuthentication) {
-		next();
-		return;
-	}
-	if (
-		window.localStorage.getItem("isLoggedIn") &&
-		window.localStorage.getItem("token")
-	) {
-		next();
-	} else {
-		next("/login");
-	}
 });
 
 export default router;
